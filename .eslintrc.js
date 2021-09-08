f3e518d4534521876a0073cf7b574c4044a6dfaf
Dylan <duanyytop@gmail.com>
2021-09-08 15:46:33 +0800

    chore: Add config and dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,139 @@
+module.exports = {
+  extends: ['airbnb', 'plugin:prettier/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react-hooks'],
+  globals: {
+    State: 'true',
+    CustomRouter: 'true',
+    echarts: 'true',
+  },
+  rules: {
+    'prettier/prettier': [
+      2,
+      {
+        printWidth: 120,
+      },
+    ],
+    semi: [2, 'never'],
+    'comma-dangle': [
+      2,
+      {
+        arrays: 'always-multiline',
+        objects: 'always-multiline',
+        imports: 'always-multiline',
+        exports: 'always-multiline',
+        functions: 'ignore',
+      },
+    ],
+    'import/no-extraneous-dependencies': [
+      2,
+      {
+        devDependencies: true,
+      },
+    ],
+    'no-unused-vars': 'off',
+    'no-undef': 'off',
+    'implicit-arrow-linebreak': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        vars: 'local',
+        args: 'after-used',
+        ignoreRestSiblings: false,
+      },
+    ],
+    'operator-linebreak': [0, 'none'],
+    'arrow-parens': [2, 'as-needed'],
+    'max-len': [
+      2,
+      {
+        code: 120,
+        ignoreComments: true,
+        ignoreTrailingComments: true,
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+        ignoreRegExpLiterals: true,
+      },
+    ],
+    'object-curly-newline': [
+      'error',
+      {
+        ObjectExpression: 'always',
+        ObjectPattern: {
+          consistent: true,
+        },
+        ImportDeclaration: {
+          consistent: true,
+        },
+        ExportDeclaration: {
+          multiline: true,
+          minProperties: 3,
+        },
+      },
+    ],
+    'no-plusplus': [0],
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.ts', '.tsx'],
+      },
+    ],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+    'react/jsx-props-no-spreading': [
+      'error',
+      {
+        html: 'ignore',
+        custom: 'ignore',
+        exceptions: [''],
+      },
+    ],
+    'react/require-default-props': [0],
+    'no-shadow': 'off',
+    'react/prop-types': [0],
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-wrap-multilines': [
+      'error',
+      {
+        prop: 'ignore',
+      },
+    ],
+    'no-console': ['error', { allow: ['error'] }],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'import/prefer-default-export': 'off',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    camelcase: [
+      'error',
+      {
+        properties: 'never',
+        ignoreDestructuring: true,
+        allow: ['^.*_'],
+      },
+    ],
+  },
+  env: {
+    jest: true,
+    browser: true,
+    node: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+}
